d45314ea71c3a6eebb348599266c091bab0e6770
moritzgladigau <moritz.gladigau@gmail.com>
2023-06-11 00:33:19 +0200

    Alle Daten vor dem Setup aus Tinkercad abgeschrieben & erklaert

--- MedDisp/MedDisp.ino
+++ MedDisp/MedDisp.ino
@@ -1,11 +1,31 @@
 //************************************************
 //Insert Libary
-#include <Keypad.h>                     //Keypad by Mark Stanley, Alexander Brevig
-#include <Adafruit_LiquidCrystal.h>     //LCD Display -> LiquidCrystal by Arduino, Adafruit
+#include <Keypad.h>                     //Keypad by Mark Stanley, Alexander Brevig. https://github.com/Chris--A/Keypad
+#include <Adafruit_LiquidCrystal.h>     //LCD Display -> LiquidCrystal by Arduino, Adafruit. https://github.com/adafruit/Adafruit_LiquidCrystal
 //************************************************
 //LCD_Diplay
 Adafruit_LiquidCrystal lcd_1(0);        //Name Dispaly as "lcd_1"
 //************************************************
+//Keypad
+const byte ROWS = 4;                    //Introduce Rows of the Keypad
+const byte COLS = 4;                    //Introduce colums of the Keypad
+
+char hexaKeys[ROWS][COLS] = {           //define the cymbols on the buttons of the keypads
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};      //row pins define
+byte colPins[COLS] = {5, 4, 3, 2};      //colum pins define
+
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);    //Difine the Keypad to the Arduino Bord
+//************************************************
+//Global variables 
+char Data[10];                          //n first undefine values named "Data(0),Data(1),...,Data(n)"
+int i;                                  //integer to count Data(n) n=i
+char customKey;                         //value that holds the last presed Key ->Defind later 
 
 void setup() {
   //LCD_Display
