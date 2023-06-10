97eb937304088655d7f090507c985597ea83cd85
Moritz Gladigau <moritz.gladigau@gmail.com>
2023-06-10 16:43:19 +0200

    Libary addons Keypad & LCD_Display

--- MedDisp/MedDisp.ino
+++ MedDisp/MedDisp.ino
@@ -1,3 +1,13 @@
+//************************************************
+//Insert Libary
+#include <Keypad.h>                     //Keypad by Mark Stanley, Alexander Brevig
+#include <Adafruit_LiquidCrystal.h>     //LCD Display -> LiquidCrystal by Arduino, Adafruit
+
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
